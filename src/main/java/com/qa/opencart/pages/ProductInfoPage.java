9bdc2844157be51a8d0b8e1817858e35e9628e5c
Manish <manish1561988@gmail.com>
2026-08-29 16:36:01 +0200

    Adding code for video 05

--- src/main/java/com/qa/opencart/pages/ProductInfoPage.java
+++ src/main/java/com/qa/opencart/pages/ProductInfoPage.java
@@ -5,13 +5,22 @@ import static com.qa.opencart.constants.AppConstants.*;
 import com.qa.opencart.utils.ElementUtil;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.sql.SQLOutput;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ProductInfoPage {
     private WebDriver driver;
     private ElementUtil elementUtil;
 
+    private Map<String, String> productMap;
     private final By productHeader = By.tagName("h1");
     private final By productImage = By.cssSelector("ul.thumbnails img");
+    private final By productMetaData = By.xpath("(//div[@id='content']//ul[@class='list-unstyled'])[1]/li");
+    private final By productPriceData = By.xpath("(//div[@id='content']//ul[@class='list-unstyled'])[2]/li");
 
     public ProductInfoPage(WebDriver driver) {
         this.driver = driver;
@@ -29,6 +38,36 @@ public class ProductInfoPage {
         System.out.println("Product Image Count: " + imageCount);
         return imageCount;
 
+    }
+
+    //Having all the product detail
+    public Map<String, String> getProductDetailsMap() {
+        productMap = new HashMap<String, String>();
+        productMap.put("productheader", getProductHeader());
+        productMap.put("productimages", String.valueOf(getProductImagesCount()));
+        getProductMetaData();
+        getProductPriceData();
+        System.out.println("Full Product Details: " + productMap);
+        return productMap;
+    }
+
+    private void getProductMetaData() {
+        List<WebElement> metaDataList = elementUtil.waitForAllElementVisible(productMetaData, DEFAULT_TIMEOUT);
+        for (WebElement e : metaDataList) {
+            String metaData = e.getText();
+            String[] meta = metaData.split(":");
+            String metaKey = meta[0].trim();
+            String metaValue = meta[1].trim();
+            productMap.put(metaKey, metaValue);
+        }
+    }
+
+    private void getProductPriceData() {
+        List<WebElement> priceList = elementUtil.waitForAllElementVisible(productPriceData, DEFAULT_TIMEOUT);
+        String productPrice = priceList.get(0).getText();
+        String exTaxPrice = priceList.get(1).getText().split(":")[1].trim();
+        productMap.put("productprice", productPrice);
+        productMap.put("extaxprice", exTaxPrice);
     }
 
 
--- src/test/java/com/qa/opencart/tests/ProductInfoPageTest.java
+++ src/test/java/com/qa/opencart/tests/ProductInfoPageTest.java
@@ -3,7 +3,11 @@ package com.qa.opencart.tests;
 import com.qa.opencart.base.BaseTest;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.util.Map;
 
 public class ProductInfoPageTest extends BaseTest {
 
@@ -12,21 +16,56 @@ public class ProductInfoPageTest extends BaseTest {
         accountsPage = loginPage.doLogin(properties.getProperty("username"), properties.getProperty("password"));
     }
 
-    @Test
-    public void productHeaderTest() {
-        searchResultPage = accountsPage.doSearch("macbook");
-        productInfoPage = searchResultPage.selectProduct("MacBook Pro");
+    @DataProvider
+    public Object[][] getProductTestData() {
+        return new Object[][]{
+                {"macbook", "MacBook Pro"},
+                {"macbook", "MacBook Air"},
+                {"imac", "iMac"},
+                {"samsung", "Samsung SyncMaster 941BW"},
+                {"samsung", "Samsung Galaxy Tab 10.1"}
+        };
+    }
+
+    @Test(dataProvider = "getProductTestData")
+    public void productHeaderTest(String searchKey, String productName) {
+        searchResultPage = accountsPage.doSearch(searchKey);
+        productInfoPage = searchResultPage.selectProduct(productName);
         String actualHeader = productInfoPage.getProductHeader();
-        Assert.assertEquals(actualHeader, "MacBook Pro");
+        Assert.assertEquals(actualHeader, productName);
+    }
+
+    @DataProvider
+    public Object[][] getProductImageTestData() {
+        return new Object[][]{
+                {"macbook", "MacBook Pro", 4},
+                {"macbook", "MacBook Air", 4},
+                {"imac", "iMac", 3},
+                {"samsung", "Samsung SyncMaster 941BW", 1},
+                {"samsung", "Samsung Galaxy Tab 10.1", 7}
+        };
+    }
+
+    @Test(dataProvider = "getProductImageTestData")
+    public void getProductImageCountTest(String searchKey, String productName, int expectedImageCount) {
+        searchResultPage = accountsPage.doSearch(searchKey);
+        productInfoPage = searchResultPage.selectProduct(productName);
+        int actualImageCount = productInfoPage.getProductImagesCount();
+        Assert.assertEquals(actualImageCount, expectedImageCount);
     }
 
     @Test
-    public void getProductImageCountTest(){
+    public void getProductInformationTest() {
         searchResultPage = accountsPage.doSearch("macbook");
         productInfoPage = searchResultPage.selectProduct("MacBook Pro");
-        int actualImageCount = productInfoPage.getProductImagesCount();
-        Assert.assertEquals(actualImageCount,4);
-    }
+        Map<String, String> actualProductMap = productInfoPage.getProductDetailsMap();
 
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(actualProductMap.get("Brand"), "Apple");
+        softAssert.assertEquals(actualProductMap.get("Product Code"), "Product 18");
+        softAssert.assertEquals(actualProductMap.get("Availability"), "Out Of Stock");
+        softAssert.assertEquals(actualProductMap.get("productprice"), "$2,000.00");
+        softAssert.assertAll();
+    }
 
 }
