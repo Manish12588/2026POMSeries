ce8b478cb0671b261f893d6227847c790f9bbd93
Manish <manish1561988@gmail.com>
2026-08-30 13:46:53 +0200

    Adding code for video 09

--- src/main/java/com/qa/opencart/factory/DriverFactory.java
+++ src/main/java/com/qa/opencart/factory/DriverFactory.java
@@ -1,6 +1,7 @@
 package com.qa.opencart.factory;
 
 import com.qa.opencart.exceptions.BrowserException;
+import com.qa.opencart.exceptions.FrameworkException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -58,14 +59,45 @@ public class DriverFactory {
      * @return
      */
     public Properties initProp() {
+        String envName = System.getProperty("env");   //Read the environment variable from command line
+        FileInputStream fis = null;
         properties = new Properties();
+
         try {
-            FileInputStream fis = new FileInputStream("./src/test/resources/config/config.properties");
-            properties.load(fis);
+            if (envName == null) {
+                System.out.println("env is null, hence running test on QA environment.");
+                fis = new FileInputStream("./src/test/resources/config/config.properties");
+            } else {
+                System.out.println("Running test on env: " + envName);
+                switch (envName.toLowerCase().trim()) {
+                    case "qa":
+                        fis = new FileInputStream("./src/test/resources/config/qa.config.properties");
+                        break;
+                    case "dev":
+                        fis = new FileInputStream("./src/test/resources/config/dev.config.properties");
+                        break;
+                    case "stage":
+                        fis = new FileInputStream("./src/test/resources/config/stage.config.properties");
+                        break;
+                    case "uat":
+                        fis = new FileInputStream("./src/test/resources/config/uat.config.properties");
+                        break;
+                    case "prod":
+                        fis = new FileInputStream("./src/test/resources/config/prod.config.properties");
+                        break;
+                    default:
+                        System.out.println("Please pass the correct environment.");
+                        throw new FrameworkException("=== INVALID ENV NAME ==== :" + envName);
+                }
+            }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
+        }
+
+        try {
+            properties.load(fis);
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
         return properties;
     }
